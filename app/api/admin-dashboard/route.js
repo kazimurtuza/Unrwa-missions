6e41dd483307be3054b790bb2c41dc06aeda5b57
Sajeeb Chakraborty <sajeebchakraborty.cse2000@gmail.com>
2024-03-05 11:31:38 +0600

    admin dashboard

--- app/api/admin-dashboard/route.js
+++ app/api/admin-dashboard/route.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
-import {Category} from "@/lib/model/category";
 import {connectionStr} from "@/lib/db";
 import {User} from "@/lib/model/users";
 import {NextResponse} from "next/server";
-import {Product} from "@/lib/model/product";
-import {Order} from "@/lib/model/order";
 
 export async function GET() {
 
@@ -12,10 +9,10 @@ export async function GET() {
     var data = {}; // Declare data as an empty object
     try {
         await mongoose.connect(connectionStr);
-        let userList = await User.countDocuments();
-        let productList = await Product.countDocuments();
-        let orderList = await Order.countDocuments();
-        let completeOrderList = await Order.countDocuments({ order_status: 3 });
+        let userList = 0;
+        let productList = 0;
+        let orderList = 0;
+        let completeOrderList = 0;
 
         data = {
             userList: userList,
