fad723bd117f7db348be3a71e0f037471de3ab9d
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-03-06 13:28:29 +0600

    all create + sidebar done

--- app/partials/Sidebar.jsx
+++ app/partials/Sidebar.jsx
@@ -824,7 +824,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                 <ul className={`pl-9 mt-1 ${!open && "hidden"}`}>
                                                     <li className="mb-1 last:mb-0">
                                                         <Link
-                                                            href="/admin/product"
+                                                            href="/admin/mission"
                                                             className={({ isActive }) =>
                                                                 "text-slate-200 block transition duration-150 truncate " +
                                                                 (isActive
@@ -841,7 +841,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
                                                     <li className="mb-1 last:mb-0">
                                                         <Link
-                                                            href="/admin/country/create"
+                                                            href="/admin/mission/create"
                                                             className={({ isActive }) =>
                                                                 "text-slate-200 block transition duration-150 truncate " +
                                                                 (isActive
@@ -956,7 +956,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
                                                         >
                                                             <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                                                Create Umrah List
+                                                                Create Umrah
                                                             </span>
                                                         </Link>
                                                     </li>
