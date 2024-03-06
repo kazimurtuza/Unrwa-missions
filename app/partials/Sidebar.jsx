af1ed24646dce3100350c53b0c7d747ab195aa7a
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-03-06 13:25:30 +0600

    all create + sidebar done

--- app/partials/Sidebar.jsx
+++ app/partials/Sidebar.jsx
@@ -944,6 +944,22 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                             </span>
                                                         </Link>
                                                     </li>
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/umrah/create"
+                                                            className={({ isActive }) =>
+                                                                "text-slate-200 block transition duration-150 truncate " +
+                                                                (isActive
+                                                                    ? "text-indigo-500"
+                                                                    : "text-slate-400 hover:text-slate-200")
+                                                            }
+
+                                                        >
+                                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Umrah List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
 
                                                     <li className="mb-1 last:mb-0">
                                                         <Link
@@ -958,6 +974,22 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                         >
                                                             <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                                                                 Premise Type
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/premise-type/create"
+                                                            className={({ isActive }) =>
+                                                                "text-slate-200 block transition duration-150 truncate " +
+                                                                (isActive
+                                                                    ? "text-indigo-500"
+                                                                    : "text-slate-400 hover:text-slate-200")
+                                                            }
+
+                                                        >
+                                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Premise Type
                                                             </span>
                                                         </Link>
                                                     </li>
