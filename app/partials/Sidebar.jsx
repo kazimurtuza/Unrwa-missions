87d2ed9a98d74a1d3bbb633613e9ce46351d2eb3
Sakib Anjum <sakib@technovicinity.com>
2024-03-15 11:52:17 +0600

    sonsole log error fixed

--- app/partials/Sidebar.jsx
+++ app/partials/Sidebar.jsx
@@ -56,6 +56,17 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         }
     }, [sidebarExpanded]);
 
+    const [hydrated, setHydrated] = React.useState(false);
+    React.useEffect(() => {
+		// This forces a rerender, so the date is rendered
+		// the second time but not the first
+		setHydrated(true);
+	}, []);
+    if (!hydrated) {
+		// Returns null on first render, so the client and server match
+		return null;
+	}
+
     return (
         <div>
             {/* Sidebar backdrop (mobile only) */}
