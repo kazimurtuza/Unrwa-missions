58523e919e30e1b1b9778c1779d834746d37d3d1
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-03-19 10:08:46 +0600

    sidebar issue

--- app/partials/Sidebar.jsx
+++ app/partials/Sidebar.jsx
@@ -630,6 +630,114 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                         </Link>
                                                     </li>
 
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/area/create"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Area
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/area"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Area List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/sub-area/create"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Sub Area
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/sub-area"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Sub Area List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/coordinate-decision/create"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Coordinate Decision
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/coordinate-decision"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Coordinate Decision List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/cargo/create"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create Cargo
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/cargo"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Cargo List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/general-status/create"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                Create General Status
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
+                                                    <li className="mb-1 last:mb-0">
+                                                        <Link
+                                                            href="/admin/general-status"
+
+                                                        >
+                                                            <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                                                General Status List
+                                                            </span>
+                                                        </Link>
+                                                    </li>
+
                                                     <li className="mb-1 last:mb-0 menu-legend">
                                                     <svg width={16} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z"/></svg>
                                                         <span className="text-sm font-medium  lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
