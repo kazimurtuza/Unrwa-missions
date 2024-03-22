c5a584676fd480f9ad7e4b492932beddc2d9cf38
Sakib Anjum <sakib@technovicinity.com>
2024-03-22 13:37:27 +0600

    unused module removed

--- app/api/loginuser/route.js
+++ app/api/loginuser/route.js
@@ -1,20 +1,12 @@
-import { NextResponse } from "next/server";
-import mongoose from "mongoose";
-import {User} from "@/lib/model/users";
-import { connectionStr } from "@/lib/db";
-import {Product} from "@/lib/model/product";
 import jwt from "jsonwebtoken";
-import {headers} from "next/headers";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
-
-export async function GET(){
-    const headerdata=headers();
+export async function GET() {
+    const headerdata = headers();
     const bearerToken = headerdata.get("authorization");
-    const token = bearerToken.split(' ')[1];
-    let info=jwt.decode(token);
-
+    const token = bearerToken.split(" ")[1];
+    let info = jwt.decode(token);
 
     return NextResponse.json(info);
-
-
 }
